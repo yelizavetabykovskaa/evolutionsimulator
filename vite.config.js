df37083e210f1ec97fa45119a655c839f6b57ebb
yelizavetabykovskaya <elizavetabykovskaa1@gmail.com>
2026-05-03 22:36:31 +0600

    add deploy script

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // ВАЖНО для GitHub Pages и папок
+  base: '/evolutionsimulator/',
   plugins: [react()],
 })
